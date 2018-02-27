39fbd0b8dd3d78bbe5cf9f99eac7fcb660bfc5ce
Valeriy Avanesov <avamesov@wias-berlin.de>
2018-02-27 19:49:15 +0100

    prediction implemented

--- HDGPRegression.py
+++ HDGPRegression.py
@@ -5,11 +5,11 @@ from scipy import linalg as la
 
 
 class HDGRegression(BaseEstimator, RegressorMixin):
-    def __init__(self, rho, sigma2):
+    def __init__(self, rho = 1, sigma2=0.001):
         self.rho = rho
         self.sigma2 = sigma2
         self.B = None
-        self.BOld = np.zeros((1,1))
+        self.BOld = np.zeros((1, 1))
         self.kOld = None
 
     def kernelMatrix(self, X, B):
@@ -17,7 +17,7 @@ class HDGRegression(BaseEstimator, RegressorMixin):
             return self.kOld
 
         B2 = B ** 2
-        nonRegularized = (self.deltas * B2[np.newaxis, np.newaxis, :]).sum(axis = 2)
+        nonRegularized = (self.deltas * B2[np.newaxis, np.newaxis, :]).sum(axis=2)
         nonRegularized = np.exp(-nonRegularized / 2)
 
         self.kOld = nonRegularized + np.identity(self.n) * self.sigma2
@@ -25,7 +25,7 @@ class HDGRegression(BaseEstimator, RegressorMixin):
         return self.kOld
 
     def kernelDerivative(self, X, K, B, component):
-        deltaSquared = self.dataSquareds[component]
+        deltaSquared = self.deltaSquareds[component]
         return -B[component] * deltaSquared * K
 
     def likelihoodDerivative(self, X, y, B):
@@ -49,10 +49,10 @@ class HDGRegression(BaseEstimator, RegressorMixin):
 
     def fit(self, X, y=None):
         self.n, self.d = X.shape
-        self.dataSquareds = [np.array([[(X[i, component] - X[j, component])**2
-                                        for i in range(0, self.n)]
-                                       for j in range(0, self.n)])
-                             for component in range(0, self.d)]
+        self.deltaSquareds = [np.array([[(X[i, component] - X[j, component]) ** 2
+                                         for i in range(0, self.n)]
+                                        for j in range(0, self.n)])
+                              for component in range(0, self.d)]
 
         self.deltas = np.array([[X[i, :] - X[j, :]
                                  for i in range(0, self.n)]
@@ -60,9 +60,26 @@ class HDGRegression(BaseEstimator, RegressorMixin):
         B = np.array([1] * X.shape[1])
 
         self.result = minimize(lambda B: self.likelihood(X, y, B),
-                 B,
-                 jac=lambda B: self.likelihoodDerivative(X, y, B),
-                 options={'gtol': 1e-3*self.d})
+                               B,
+                               jac=lambda B: self.likelihoodDerivative(X, y, B),
+                               options={'gtol': 1e-3*self.d})
 
         self.B = self.result.x
+        print(self.B)
+        self.alpha = np.linalg.inv(self.kernelMatrix(X, self.B)) @ y
+        self.X = X
         return self
+
+    def crossKernel(self, X, Xtrain):
+        result = np.zeros((X.shape[0], Xtrain.shape[0]))
+        for i in range(0, X.shape[0]):
+            for j in range(0, Xtrain.shape[0]):
+                result[i][j] = (((X[i, :] - Xtrain[j, :])*self.B)**2).sum()
+        return np.exp(-result / 2)
+
+
+    def predict(self, Xtest, y=None):
+        kstar = self.crossKernel(self.X, Xtest)
+        return kstar.T @ self.alpha
+
+
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from HDGPRegression import HDGRegression
 import numpy as np
 import scipy
 from sklearn import datasets
-
-r = HDGRegression(1, 0.2)
+from sklearn.model_selection import cross_val_score
+from sklearn import preprocessing
+from sklearn.gaussian_process import GaussianProcessRegressor
 
 X = np.array([[0, 0], [1, 1], [2, 2]])
 y = np.array([1, 2, 3]).reshape(-1, 1)
@@ -11,6 +12,15 @@ y = np.array([1, 2, 3]).reshape(-1, 1)
 diabetes = datasets.load_diabetes()
 X = diabetes.data
 y = diabetes.target.reshape(-1, 1)
+X = preprocessing.scale(X)
 
-print(r.fit(X,y))
-print(r.B)
+r = HDGRegression(rho=1e100)
+
+print(y.var())
+print(cross_val_score(r, X, y, cv = 4, scoring='neg_mean_squared_error', n_jobs=4))
+lr = GaussianProcessRegressor()
+print(cross_val_score(lr, X, y, cv = 4, scoring='neg_mean_squared_error'))
+
+# print(r.fit(X,y))
+# xtest = X[0:2, :]
+# print(r.predict(xtest))
